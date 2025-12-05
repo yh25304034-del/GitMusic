fb364e54d5ce1f500f556be751d84af4122a713a
hayashiyuma-admin <yh25304034@ga.ttc.ac.jp>
2025-12-05 14:52:10 +0900

    hayashi commit

---
+++ WEB-INF/MusicInput.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ユーザー登録</title>
+    </head>
+    <body>
+        <h1>ユーザー登録</h1>
+        <form method="POST" action="MusicServlet">
+            <input type="text" name="name"><br>
+            <input type="text" name="music"><br>
+            <input type="submit" name="登録" />
+        </form>
+    </body>
+</html>
---
+++ WEB-INF/classes/MusicServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MusicServlet extends HttpServlet {
+            protected void doPost(HttpServletRequest request,HttpServletResponse response) 
+                throws IOException,ServletException {
+                    request.setCharacterEncoding("UTF-8");
+
+                    String username = request.getParameter("name");
+                    String FavoriteMusic = request.getParameter("music");
+
+                    request.setAttribute("name", username);
+                    request.setAttribute("music",FavoriteMusic);
+
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("MusicOutput");
+
+                    dispatcher.forward(request,response);
+            }
+}
